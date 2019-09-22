4d1d2e2df22daae249b8eb9d4e2d7c693085e01e
Mark Findley <mark_findley@hotmail.com>
2019-09-22 09:47:58 -0500

    SQL Homework Submission

---
+++ EmployeeSQL/SQLHomeworkQueries.sql
@@ -0,0 +1,61 @@
+﻿-- List the following details of each employee: employee number, last name, first name, gender, and salary.
+select e.EmpNo, e.LastName, e.FirstName, e.Gender, s.Salary
+from Employees as e 
+join Salaries as s
+on e.EmpNo = s.EmpNo;
+ 
+-- List employees who were hired in 1986.
+select EmpNo, LastName, FirstName, HireDate
+from Employees
+where HireDate between '1986-01-01' and '1986-12-31';
+
+--List the manager of each department with the following information: department number, department name, 
+--the manager's employee number, last name, first name, and start and end employment dates.
+select d.DeptNo, d.DeptName, e.EmpNo, e.LastName, e.FirstName, m.FromDate, m.ToDate 
+from Departments as d
+join DeptManager as m
+on d.DeptNo = m.DeptNo
+--and m.ToDate = '9999-01-01' -- uncomment this if you only want employees who are currently managing a department
+join Employees as e
+on m.EmpNo = e.EmpNo;
+
+-- List the department of each employee with the following information: employee number, last name, first name, and department name.
+select e.EmpNo, e.LastName, e.FirstName, d.DeptName
+from Employees as e
+join DeptEmp as de
+on e.EmpNo = de.EmpNo
+--and de.ToDate = '9999-01-01' -- uncomment this if you only want the departments an employee is currently working in
+join Departments d
+on de.DeptNo = d.DeptNo;
+
+--List all employees whose first name is Hercules and last names begin with B.
+select EmpNo, LastName, FirstName
+from Employees as e
+where FirstName = 'Hercules'
+and LastName like 'B%';
+
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.
+select e.EmpNo, e.LastName, e.FirstName, d.DeptName
+from Employees as e
+join DeptEmp as de
+on e.EmpNo = de.EmpNo
+--and de.ToDate = '9999-01-01' -- uncomment this if you only want employees currently working in Sales
+join Departments d
+on de.DeptNo = d.DeptNo
+and d.DeptName = 'Sales';
+
+--List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+select e.EmpNo, e.LastName, e.FirstName, d.DeptName
+from Employees as e
+join DeptEmp as de
+on e.EmpNo = de.EmpNo
+--and de.ToDate = '9999-01-01' -- uncomment this if you only want employees currently working in Sales or Development
+join Departments d
+on de.DeptNo = d.DeptNo
+and d.DeptName in ('Sales','Development');
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+select LastName, count(*) as LastNameCount
+from Employees
+group by LastName
+order by count(*) desc; 
---
+++ EmployeeSQL/SQLHomeworkSchema.sql
@@ -0,0 +1,92 @@
+﻿--DROP TABLE Employees
+--DROP TABLE Salaries 
+--DROP TABLE Titles 
+--DROP TABLE Departments 
+--DROP TABLE DeptEmp 
+--DROP TABLE DeptManager
+
+CREATE TABLE Employees (
+    EmpNo int   NOT NULL,
+    BirthDate date   NOT NULL,
+    FirstName varchar(100)   NOT NULL,
+    LastName varchar(100)   NOT NULL,
+    Gender char(1)   NOT NULL,
+    HireDate date   NOT NULL,
+    CONSTRAINT pk_Employees PRIMARY KEY (
+        EmpNo
+     )
+);
+
+CREATE TABLE Salaries (
+    EmpNo int   NOT NULL,
+    Salary money   NOT NULL,
+    FromDate date   NOT NULL,
+    ToDate date   NOT NULL,
+    CONSTRAINT pk_Salaries PRIMARY KEY (
+        EmpNo, FromDate
+     )
+);
+
+CREATE TABLE Titles (
+    EmpNo int   NOT NULL,
+    Title varchar(100)   NOT NULL,
+    FromDate date   NOT NULL,
+    ToDate date   NOT NULL,
+    CONSTRAINT pk_Titles PRIMARY KEY (
+        EmpNo, FromDate
+     )
+);
+
+CREATE TABLE Departments (
+    DeptNo char(4) NOT NULL,
+    DeptName varchar(100)   NULL,
+    CONSTRAINT pk_Departments PRIMARY KEY (
+        DeptNo
+     )
+);
+
+CREATE TABLE DeptEmp (
+    EmpNo int   NOT NULL,
+    DeptNo char(4)  NOT NULL,
+    FromDate date   NOT NULL,
+    ToDate date   NOT NULL,
+    CONSTRAINT pk_DeptEmp PRIMARY KEY (
+        EmpNo, DeptNo, FromDate
+     )
+);
+
+CREATE TABLE DeptManager (
+    DeptNo char(4)  NOT NULL,
+    EmpNo int   NOT NULL,
+    FromDate date   NOT NULL,
+    ToDate date   NOT NULL,
+    CONSTRAINT pk_DeptManager PRIMARY KEY (
+        DeptNo, EmpNo, FromDate
+     )
+);
+
+ALTER TABLE Salaries ADD CONSTRAINT fk_Salaries_EmpNo FOREIGN KEY(EmpNo)
+REFERENCES Employees (EmpNo);
+
+ALTER TABLE Titles ADD CONSTRAINT fk_Titles_EmpNo FOREIGN KEY(EmpNo)
+REFERENCES Employees (EmpNo);
+
+ALTER TABLE DeptEmp ADD CONSTRAINT fk_DeptEmp_EmpNo FOREIGN KEY(EmpNo)
+REFERENCES Employees (EmpNo);
+
+ALTER TABLE DeptEmp ADD CONSTRAINT fk_DeptEmp_DeptNo FOREIGN KEY(DeptNo)
+REFERENCES Departments (DeptNo);
+
+ALTER TABLE DeptManager ADD CONSTRAINT fk_DeptManager_DeptNo FOREIGN KEY(DeptNo)
+REFERENCES Departments (DeptNo);
+
+ALTER TABLE DeptManager ADD CONSTRAINT fk_DeptManager_EmpNo FOREIGN KEY(EmpNo)
+REFERENCES Employees (EmpNo);
+
+-- now import the tables
+select count(*) from Employees
+select count(*) from Salaries 
+select count(*) from Titles 
+select count(*) from Departments 
+select count(*) from DeptEmp 
+select count(*) from DeptManager 
